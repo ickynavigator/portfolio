d4413d1da28acac550fa53613c4d1414646add69
Obi Fortune <gabrielobi.of@gmail.com>
2024-04-06 09:52:09 -0400

    add post schema to the sanity configuration

--- src/sanity/schema/index.ts
+++ src/sanity/schema/index.ts
@@ -5,7 +5,8 @@ import personalInfo from '~/sanity/schema/documents/personalInfo';
 import category from '~/sanity/schema/documents/category';
 import blockContent from '~/sanity/schema/objects/blockContent';
 import socialLink from '~/sanity/schema/objects/socialLink';
+import post from '~/sanity/schema/documents/post';
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [configuration, personalInfo, category, blockContent, socialLink],
+  types: [configuration, personalInfo, category, blockContent, socialLink, post],
 };
