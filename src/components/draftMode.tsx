a881576d7081b7b7dd890dc0f50ae1b95736da27
Obi Fortune <gabrielobi.of@gmail.com>
2024-08-12 22:37:33 -0400

    refactor draftmode

--- src/components/draftMode.tsx
+++ src/components/draftMode.tsx
@@ -1,28 +1,28 @@
-import { Affix, Anchor, Button, rem } from '@mantine/core';
-import { IconPencil } from '@tabler/icons-react';
+import { ActionIcon, Affix, Anchor, Tooltip, rem } from '@mantine/core';
+import { IconPencilOff } from '@tabler/icons-react';
 import { VisualEditing } from 'next-sanity';
 import { draftMode } from 'next/headers';
-
-function DraftModeDisableButton() {
-  return (
-    <Affix position={{ bottom: 20, right: 20 }}>
-      <Anchor href="/api/draft-mode/disable">
-        <Button leftSection={<IconPencil style={{ width: rem(16), height: rem(16) }} />}>
-          Disable preview mode
-        </Button>
-      </Anchor>
-    </Affix>
-  );
-}
 
 export default function DraftMode() {
   const isDraftMode = draftMode().isEnabled;
 
+  if (!isDraftMode) {
+    return null;
+  }
+
   return (
     <>
-      {isDraftMode && <DraftModeDisableButton />}
+      <Affix position={{ bottom: 20, right: 20 }}>
+        <Anchor href="/api/draft-mode/disable">
+          <Tooltip label="Disable Preview mode" withArrow>
+            <ActionIcon variant="filled" aria-label="Disable Preview mode" color="red">
+              <IconPencilOff style={{ width: rem(16), height: rem(16) }} />
+            </ActionIcon>
+          </Tooltip>
+        </Anchor>
+      </Affix>
 
-      {isDraftMode && <VisualEditing />}
+      <VisualEditing />
     </>
   );
 }
