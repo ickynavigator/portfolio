f80282d380a118716ae52ff6e1e04bf6a3aab6ea
Obi Fortune <gabrielobi.of@gmail.com>
2024-08-12 22:25:00 -0400

    update portable text ff

--- src/components/customPortableText.tsx
+++ src/components/customPortableText.tsx
@@ -2,6 +2,7 @@ import { CodeHighlightTabs } from '@mantine/code-highlight';
 import { Anchor, Blockquote, Box, Code, List, ListItem, Text, Title } from '@mantine/core';
 import { PortableText, type PortableTextReactComponents } from '@portabletext/react';
 import { getFileIcon } from '~/lib/fileIcon';
+import { dmSans, syne } from '~/lib/font';
 
 const defaultPortableTextComponents: PortableTextReactComponents = {
   types: {
@@ -29,14 +30,38 @@ const defaultPortableTextComponents: PortableTextReactComponents = {
   },
 
   block: {
-    normal: ({ children }) => <Text>{children}</Text>,
-    blockquote: ({ children }) => <Blockquote>{children}</Blockquote>,
-    h1: ({ children }) => <Title order={1}>{children}</Title>,
-    h2: ({ children }) => <Title order={2}>{children}</Title>,
-    h3: ({ children }) => <Title order={3}>{children}</Title>,
-    h4: ({ children }) => <Title order={4}>{children}</Title>,
-    h5: ({ children }) => <Title order={5}>{children}</Title>,
-    h6: ({ children }) => <Title order={6}>{children}</Title>,
+    normal: ({ children }) => <Text ff={dmSans.style.fontFamily}>{children}</Text>,
+    blockquote: ({ children }) => <Blockquote ff={dmSans.style.fontFamily}>{children}</Blockquote>,
+    h1: ({ children }) => (
+      <Title order={1} ff={syne.style.fontFamily}>
+        {children}
+      </Title>
+    ),
+    h2: ({ children }) => (
+      <Title order={2} ff={syne.style.fontFamily}>
+        {children}
+      </Title>
+    ),
+    h3: ({ children }) => (
+      <Title order={3} ff={syne.style.fontFamily}>
+        {children}
+      </Title>
+    ),
+    h4: ({ children }) => (
+      <Title order={4} ff={syne.style.fontFamily}>
+        {children}
+      </Title>
+    ),
+    h5: ({ children }) => (
+      <Title order={5} ff={syne.style.fontFamily}>
+        {children}
+      </Title>
+    ),
+    h6: ({ children }) => (
+      <Title order={6} ff={syne.style.fontFamily}>
+        {children}
+      </Title>
+    ),
   },
 
   marks: {
