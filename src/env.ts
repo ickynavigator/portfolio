b8d8c5c47a63e3cd3ad8f42d8f25860ae0cd3e19
Obi Fortune <gabrielobi.of@gmail.com>
2024-08-12 23:47:20 -0400

    add base process.env to the t3 env vars list

--- src/env.ts
+++ src/env.ts
@@ -3,6 +3,21 @@ import { vercel } from '@t3-oss/env-nextjs/presets';
 import { z } from 'zod';
 import { sanity } from '~/lib/sanity/env.preset';
 
+const base = () => {
+  return createEnv({
+    client: {},
+    server: {},
+    shared: {
+      NODE_ENV: z.enum(['development', 'production', 'test']).default('development'),
+      TZ: z.string().optional(),
+    },
+    runtimeEnv: {
+      NODE_ENV: process.env.NODE_ENV,
+      TZ: process.env.TZ,
+    },
+  });
+};
+
 const env = createEnv({
   client: {
     NEXT_PUBLIC_SANITY_API_VERSION: z.string().default('2024-04-03'),
@@ -10,7 +25,7 @@ const env = createEnv({
   },
   server: {},
   shared: {},
-  extends: [vercel(), sanity()],
+  extends: [vercel(), sanity(), base()],
   experimental__runtimeEnv: {
     NEXT_PUBLIC_SANITY_API_VERSION: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
     NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL,
