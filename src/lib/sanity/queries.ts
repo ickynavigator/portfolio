0b29d9211c63287514c9ad7f2059f21b3773bd11
Obi Fortune <gabrielobi.of@gmail.com>
2024-11-04 20:45:14 -0500

    basic blog posts setup

---
+++ src/lib/sanity/queries.ts
@@ -0,0 +1,39 @@
+import { defineQuery } from "groq";
+
+// @sanity-typegen-ignore
+export const POSTS_QUERY = defineQuery(`
+    *[_type == "post" && defined(slug.current) && hidden != true] | order(postedAt desc) {
+        _id,
+        title,
+        slug,
+        postedAt, 
+        image,
+        "wordCount": length(pt::text(body))
+    }
+`);
+
+export const PAGINATED_POSTS_QUERY = defineQuery(`
+    {
+        "data": ${POSTS_QUERY},
+    }
+`);
+// TODO: Implement pagination
+// "pagination": {
+//     "page": select(
+//         $offset >= count(${POSTS_QUERY}) => -1,
+//         round($offset / $limit) + 1
+//     ),
+//     "per_page": round($limit),
+//     "page_count": count(${POSTS_QUERY}),
+//     "total_count": count(${POSTS_QUERY}),
+// }
+
+export const POST_SLUGS_QUERY = defineQuery(`
+    *[_type == "post" && defined(slug.current) && hidden != true] {
+        "slug": slug.current
+    }
+`);
+
+export const POST_QUERY = defineQuery(`
+  *[_type == "post" && slug.current == $slug && hidden != true][0]
+`);
