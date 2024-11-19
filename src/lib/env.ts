ba46031f51448ef8e1a11d1c63dcf9c9ab6965cf
Obi Fortune <gabrielobi.of@gmail.com>
2024-11-19 12:11:06 -0500

    move envs to shared variable

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -5,14 +5,14 @@ type RuntimeEnv = Record<string, string | boolean | number | undefined>;
 export const getEnv = (runtimeEnv: RuntimeEnv) => {
   return createEnv({
     clientPrefix: "PUBLIC_",
-    client: {
-      PUBLIC_SANITY_API_PROJECT_ID: z.string().min(1),
-      PUBLIC_SANITY_API_DATASET: z.string().min(1).default("production"),
-    },
+    client: {},
     server: {
       SANITY_API_WRITE_TOKEN: z.string().min(1),
     },
-    shared: {},
+    shared: {
+      PUBLIC_SANITY_API_PROJECT_ID: z.string().min(1),
+      PUBLIC_SANITY_API_DATASET: z.string().min(1).default("production"),
+    },
     runtimeEnv: runtimeEnv,
     emptyStringAsUndefined: true,
   });
