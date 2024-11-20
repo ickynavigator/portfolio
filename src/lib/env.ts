7a6da13d29b4bdad4c4d42bc829aab46281810c5
Obi Fortune <gabrielobi.of@gmail.com>
2024-11-20 09:30:02 -0500

    add default project ID

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -10,7 +10,7 @@ export const getEnv = (runtimeEnv: RuntimeEnv) => {
       SANITY_API_WRITE_TOKEN: z.string().min(1),
     },
     shared: {
-      PUBLIC_SANITY_API_PROJECT_ID: z.string().min(1),
+      PUBLIC_SANITY_API_PROJECT_ID: z.string().min(1).default("gtsyvuts"),
       PUBLIC_SANITY_API_DATASET: z.string().min(1).default("production"),
       PUBLIC_SANITY_API_VERSION: z.string().min(1).default("2022-03-07"),
     },
