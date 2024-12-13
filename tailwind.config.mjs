682649e4570c64fa03cc59ec3c22453296885371
Obi Fortune <gabrielobi.of@gmail.com>
2024-12-13 13:34:41 -0500

    use media for darkmode instead

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,7 +4,7 @@ import tailwindcssAnimatePlugin from "tailwindcss-animate";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: ["class"],
+  darkMode: "media",
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
