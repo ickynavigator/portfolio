0e3e30fba420a666f677fc36c95f65b7a4269be8
Obi Fortune <gabrielobi.of@gmail.com>
2024-12-14 22:42:46 -0500

    feat: add HardBreak component and improve heading slug generation

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function getTextFromPortableTextNode(node?: { text?: string }[]) {
+  return (node ?? []).reduce((acc, curr) => (acc += curr?.text), "");
+}
