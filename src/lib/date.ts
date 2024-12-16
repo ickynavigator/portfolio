66c858aa30936e6fc80ef3355279cb9d5dcfe090
Obi Fortune <gabrielobi.of@gmail.com>
2024-12-15 21:52:55 -0500

    feat: add blog list layout with date and reading time display

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -1,10 +1,24 @@
 import dayjs from "dayjs";
+import duration from "dayjs/plugin/duration";
 import localizedFormat from "dayjs/plugin/localizedFormat";
+import relativeTime from "dayjs/plugin/relativeTime";
 
+dayjs.extend(duration);
+dayjs.extend(relativeTime);
 dayjs.extend(localizedFormat);
 
 export function formatDateWithLocalized(date: string) {
   return dayjs(date).format("ll");
+}
+
+export function getReadingTime(
+  textLength: number,
+  wordsPerMinute = 200,
+  averageWordLength = 5,
+) {
+  const estimatedWordCount = textLength / averageWordLength;
+  const readingTime = estimatedWordCount / wordsPerMinute;
+  return dayjs.duration(readingTime, "minutes").humanize();
 }
 
 export default dayjs;
