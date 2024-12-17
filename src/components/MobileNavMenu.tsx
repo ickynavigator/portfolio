0db90baa49c4f6695a57975971da326babe461b6
Obi Fortune <gabrielobi.of@gmail.com>
2024-12-17 15:57:50 -0500

    feat: integrate ThemeSwitcher into NavLinks and add MobileNavMenu component

---
+++ src/components/MobileNavMenu.tsx
@@ -0,0 +1,36 @@
+import { Menu } from "lucide-react";
+import React from "react";
+
+import {
+  Drawer,
+  DrawerContent,
+  DrawerDescription,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "~/components/ui/drawer";
+
+interface Props {
+  children?: React.ReactNode;
+}
+
+const MobileNavMenu = (props: Props) => {
+  return (
+    <Drawer>
+      <DrawerTrigger>
+        <Menu />
+      </DrawerTrigger>
+
+      <DrawerContent>
+        <DrawerHeader className="text-center">
+          <DrawerTitle>Obi Fortune</DrawerTitle>
+          <DrawerDescription className="sr-only">Menu</DrawerDescription>
+        </DrawerHeader>
+
+        {props.children}
+      </DrawerContent>
+    </Drawer>
+  );
+};
+
+export default MobileNavMenu;
