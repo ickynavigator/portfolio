99df35b683d8388e02c07f54c15a1bd586a3c95a
Obi Fortune <gabrielobi.of@gmail.com>
2024-12-17 18:36:32 -0500

    setup theme switching for tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,7 +4,7 @@ import tailwindcssAnimatePlugin from "tailwindcss-animate";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: "media",
+  darkMode: "selector",
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
