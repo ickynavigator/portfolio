7d6ee6557ba40f782623f72bf887262f17123dd3
Obi Fortune <gabrielobi.of@gmail.com>
2024-12-26 14:52:20 -0500

    feat: add Cloudflare environment variables for build configuration

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -16,5 +16,36 @@ export const getEnv = (runtimeEnv: RuntimeEnv) => {
     },
     runtimeEnv: runtimeEnv,
     emptyStringAsUndefined: true,
+    extends: [cloudflare(runtimeEnv)],
   });
 };
+
+/**
+ * @see https://developers.cloudflare.com/pages/configuration/build-configuration/#environment-variables
+ */
+export const cloudflare = (runtimeEnv: RuntimeEnv = process.env) =>
+  createEnv({
+    server: {
+      /**
+       * @description `1`
+       * @example Changing build behaviour when run on Pages versus locally
+       */
+      CF_PAGES: z.string().optional(),
+      /**
+       * @description `<sha1-hash-of-current-commit>`
+       * @example Passing current commit ID to error reporting, for example, Sentry
+       */
+      CF_PAGES_COMMIT_SHA: z.string().optional(),
+      /**
+       * @description `<branch-name-of-current-deployment>`
+       * @example Customizing build based on branch, for example, disabling debug logging on `production`
+       */
+      CF_PAGES_BRANCH: z.string().optional(),
+      /**
+       * @description `<url-of-current-deployment>`
+       * @example Allowing build tools to know the URL the page will be deployed at
+       */
+      CF_PAGES_URL: z.string().optional(),
+    },
+    runtimeEnv,
+  });
