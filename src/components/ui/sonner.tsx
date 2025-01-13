5c5470b8c95b6e01c38e677fd24864d48390fb6d
Obi Fortune <gabrielobi.of@gmail.com>
2025-01-12 22:32:07 -0500

    feat: integrate Sonner for toast notifications and add toasts on code copy

---
+++ src/components/ui/sonner.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import { Toaster as Sonner } from "sonner";
+
+const useGetTheme = () => {
+  const [isDark, setIsDark] = useState(true);
+
+  useEffect(() => {
+    const abortController = new AbortController();
+
+    document.addEventListener("themechange", () => {}, {
+      signal: abortController.signal,
+    });
+
+    return () => {
+      abortController.abort();
+    };
+  }, []);
+
+  return isDark;
+};
+
+type ToasterProps = React.ComponentProps<typeof Sonner>;
+
+const Toaster = ({ ...props }: ToasterProps) => {
+  const isDark = useGetTheme();
+
+  return (
+    <Sonner
+      theme={isDark ? "dark" : "light"}
+      className="toaster group"
+      toastOptions={{
+        classNames: {
+          toast:
+            "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
+          description: "group-[.toast]:text-muted-foreground",
+          actionButton:
+            "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
+          cancelButton:
+            "group-[.toast]:bg-muted group-[.toast]:text-muted-foreground",
+        },
+      }}
+      {...props}
+    />
+  );
+};
+
+export { Toaster };
