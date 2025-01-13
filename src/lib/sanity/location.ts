b93253d5b81140ce4958f15cfc42537f81c90934
Obi Fortune <gabrielobi.of@gmail.com>
2025-01-12 21:41:11 -0500

    feat: define visual editing locations for blog, project, and career

--- src/lib/sanity/location.ts
+++ src/lib/sanity/location.ts
@@ -1,9 +1,37 @@
-import { type PresentationPluginOptions } from "sanity/presentation";
+import {
+  defineLocations,
+  type PresentationPluginOptions,
+} from "sanity/presentation";
 
 type Locations = NonNullable<
   NonNullable<PresentationPluginOptions["resolve"]>["locations"]
 >;
 
-const resolver: Locations = {};
+const resolver: Locations = {
+  post: defineLocations({
+    select: { title: "title", slug: "slug.current" },
+    resolve: (doc) => ({
+      locations: [
+        { title: doc?.title || "Untitled", href: `/blog/${doc?.slug}` },
+        { title: "Posts", href: "/blog" },
+      ],
+    }),
+  }),
+  project: defineLocations({
+    select: { title: "title", slug: "slug.current" },
+    resolve: (doc) => ({
+      locations: [
+        { title: doc?.title || "Untitled", href: `/project/${doc?.slug}` },
+        { title: "Projects", href: "/project" },
+      ],
+    }),
+  }),
+  career: defineLocations({
+    select: {},
+    resolve: () => ({
+      locations: [{ title: "Career", href: "/career" }],
+    }),
+  }),
+};
 
 export default resolver;
