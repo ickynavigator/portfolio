9b4eae312a7897d54c021aa02653ddfa65b6ef2f
Obi Fortune <gabrielobi.of@gmail.com>
2025-04-03 00:39:23 -0400

    fix: update styled-components theme integration and adjust padding in BlockQuote component

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,16 @@
 /// <reference types="@sanity/astro/module" />
+
+import "styled-components";
+
 type Runtime = import("@astrojs/cloudflare").Runtime<Env>;
+type Theme = import("@sanity/ui/theme").Theme;
 
 declare namespace App {
   // eslint-disable-next-line @typescript-eslint/no-empty-object-type
   interface Locals extends Runtime {}
+}
+
+declare module "styled-components" {
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+  export interface DefaultTheme extends Theme {}
 }
