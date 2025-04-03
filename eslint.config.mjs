bc9ec4e39162dae8e3c86c153bfba7c6fd8f4d95
Obi Fortune <gabrielobi.of@gmail.com>
2025-04-03 02:25:24 -0400

    chore(eslint): disable @typescript-eslint/no-empty-object-type rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -30,6 +30,7 @@ export default tseslint.config(
       "no-unused-vars": "off",
       "@typescript-eslint/no-unused-vars": "off",
       "unused-imports/no-unused-imports": "error",
+      "@typescript-eslint/no-empty-object-type": "off",
       "unused-imports/no-unused-vars": [
         "warn",
         {
