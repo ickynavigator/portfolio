37ddcb7a31485a3820d96e0a7b9ca7542ee5d3c4
Obi Fortune <gabrielobi.of@gmail.com>
2025-04-03 02:41:20 -0400

    chore(types): remove unused TypeScript error suppression in BirthdayAnnouncement and env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -6,11 +6,9 @@ type Runtime = import("@astrojs/cloudflare").Runtime<Env>;
 type Theme = import("@sanity/ui/theme").Theme;
 
 declare namespace App {
-  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
   interface Locals extends Runtime {}
 }
 
 declare module "styled-components" {
-  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
   export interface DefaultTheme extends Theme {}
 }
