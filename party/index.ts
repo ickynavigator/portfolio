fb53966857598e57e9a22e1b2fb55849d481c6b9
Obi Fortune <gabrielobi.of@gmail.com>
2025-04-06 15:08:57 -0400

    feat: add partykit server

---
+++ party/index.ts
@@ -0,0 +1,56 @@
+import type * as Party from "partykit/server";
+
+const partyMessage = (people: number) => {
+  let message;
+
+  switch (people) {
+    case 0:
+      message = `No one is here yet. Let's get this party started! 🎉`;
+      break;
+    case 1:
+      message = `It's just you here, but that's still a party! 🎈`;
+      break;
+    default:
+      message = `You're partying with ${people} people right now! 🎈`;
+      break;
+  }
+
+  return message;
+};
+
+export default class Server implements Party.Server {
+  people: number;
+
+  constructor(readonly party: Party.Room) {
+    this.people = 0;
+  }
+
+  onConnect(conn: Party.Connection, ctx: Party.ConnectionContext) {
+    this.people += 1;
+
+    console.log(
+      `Connected:
+id:     ${conn.id}
+room:   ${this.party.id}
+people: ${this.people}
+url:    ${new URL(ctx.request.url).pathname}`,
+    );
+
+    this.party.broadcast(partyMessage(this.people), [conn.id]);
+  }
+
+  onClose(conn: Party.Connection) {
+    this.people -= 1;
+
+    console.log(
+      `Disconnected:
+id:     ${conn.id}
+room:   ${this.party.id}
+people: ${this.people}`,
+    );
+
+    this.party.broadcast(partyMessage(this.people));
+  }
+}
+
+Server satisfies Party.Worker;
