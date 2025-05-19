8b55625a1b5e9b3400579fd4b404c3de6bca04e6
Obi Fortune <gabrielobi.of@gmail.com>
2025-05-19 11:08:42 -0400

    feat: implement getBase function for enhanced meta data handling

---
+++ src/lib/meta.base.ts
@@ -0,0 +1,60 @@
+import type { MetaBase } from "~/lib/meta";
+import { loadQuery } from "~/lib/sanity/load-query";
+import { PROFILE_IMAGE_QUERY } from "~/lib/sanity/queries";
+
+const getBase: MetaBase = async (Astro) => {
+  const profilePicture =
+    (await loadQuery({ query: PROFILE_IMAGE_QUERY })).result?.url ??
+    "/favicon.ico";
+
+  const appURL = new URL(Astro.url.pathname, Astro.site);
+
+  const baseTitle = "Obi Fortune's Portfolio";
+  const baseDescription =
+    "Obi Fortune's Portfolio. Built with Sanity, Astro, and Tailwind CSS.";
+
+  return {
+    title: baseTitle,
+    description: baseDescription,
+    creator: "Obi Fortune",
+    author: "Obi Fortune",
+    keywords: [
+      "Astro",
+      "TypeScript",
+      "Sanity",
+      "Blog",
+      "Portfolio",
+      "Obi Fortune",
+      "Javascript",
+      "Tech",
+      "Technology",
+      "Programming",
+      "Web Development",
+    ],
+    application_name: "Obi Fortune's Portfolio",
+
+    og: {
+      image: profilePicture,
+      title: baseTitle,
+      type: "website",
+      url: appURL,
+      site_name: "Obi Fortune's Portfolio",
+      locale: "en_US",
+      description: baseDescription,
+    },
+
+    twitter: {
+      title: baseTitle,
+      description: baseDescription,
+      site: appURL,
+      image: profilePicture,
+      card: "summary",
+      creator: "@obifortunebleh",
+      image_alt: "Obi Fortune's Portfolio",
+      creatorid: "@obifortunebleh",
+      site_id: "@obifortunebleh",
+    },
+  };
+};
+
+export default getBase;
