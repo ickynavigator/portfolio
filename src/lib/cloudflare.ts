a30c46bdd695e4b91a671076f710a3cd78051950
Obi Fortune <gabrielobi.of@gmail.com>
2025-05-19 22:07:50 -0400

    refactor: inline getZoneId function and use env variable directly in revalidateTag

--- src/lib/cloudflare.ts
+++ src/lib/cloudflare.ts
@@ -12,17 +12,12 @@ export function getCloudflareInstance() {
   };
 }
 
-export function getZoneId() {
-  const env = getEnv();
-
-  return env.CF_ZONE_ID;
-}
-
 export async function revalidateTag(tags: string[]) {
+  const env = getEnv();
   const c = getCloudflareInstance();
 
   return await c.cloudflare.cache.purge({
-    zone_id: getZoneId(),
+    zone_id: env.CF_ZONE_ID,
     tags: tags,
   });
 }
