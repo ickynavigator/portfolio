b9408b3798f946b25568fca3fc84c087d520c3d5
Obi Fortune <gabrielobi.of@gmail.com>
2025-05-20 01:00:01 -0400

    fix: change payload retrieval method from json to text in validateSignature function

--- src/lib/sanity/revalidate.ts
+++ src/lib/sanity/revalidate.ts
@@ -6,7 +6,7 @@ export async function validateSignature(req: Request) {
   const env = getEnv();
 
   const signature = req.headers.get(SIGNATURE_HEADER_NAME);
-  const stringifiedPayload = await req.json<string>();
+  const stringifiedPayload = await req.text();
 
   if (!signature) {
     return null;
