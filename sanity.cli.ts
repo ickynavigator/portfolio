857411c9f082a315540212bb5ff14aec0ac644dc
Obi Fortune <gabrielobi.of@gmail.com>
2025-07-20 22:06:30 -0400

    add sanity deploy command

---
+++ sanity.cli.ts
@@ -0,0 +1,9 @@
+import { defineCliConfig } from "sanity/cli";
+
+export default defineCliConfig({
+  api: {
+    projectId: "gtsyvuts",
+    dataset: "production",
+  },
+  studioHost: "obifortune",
+});
