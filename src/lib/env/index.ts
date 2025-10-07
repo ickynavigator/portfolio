14a7745817baab79e40c443e7e05cacc688cf29b
Obi Fortune <gabrielobi.of@gmail.com>
2025-10-07 09:26:47 -0400

    update cloudflare setup

--- src/lib/env/index.ts
+++ src/lib/env/index.ts
@@ -1,6 +1,8 @@
 import { createEnv } from "@t3-oss/env-core";
 import { z } from "zod";
 
+import { cloudflare } from "./preset/cloudflare";
+
 type RuntimeEnv = Record<string, string | boolean | number | undefined>;
 export const getEnv = (runtimeEnv: RuntimeEnv) => {
   return createEnv({
@@ -28,36 +30,3 @@ export const getEnv = (runtimeEnv: RuntimeEnv) => {
     extends: [cloudflare(runtimeEnv)],
   });
 };
-
-/**
- * @see https://developers.cloudflare.com/pages/configuration/build-configuration/#environment-variables
- */
-export const cloudflare = (runtimeEnv: RuntimeEnv = process.env) =>
-  createEnv({
-    server: {
-      CI: z.stringbool().optional().default(false),
-      /**
-       * @description `1`
-       * @example Changing build behaviour when run on Pages versus locally
-       */
-      CF_PAGES: numberish.optional(),
-      /**
-       * @description `<sha1-hash-of-current-commit>`
-       * @example Passing current commit ID to error reporting, for example, Sentry
-       */
-      CF_PAGES_COMMIT_SHA: z.string().optional(),
-      /**
-       * @description `<branch-name-of-current-deployment>`
-       * @example Customizing build based on branch, for example, disabling debug logging on `production`
-       */
-      CF_PAGES_BRANCH: z.string().optional(),
-      /**
-       * @description `<url-of-current-deployment>`
-       * @example Allowing build tools to know the URL the page will be deployed at
-       */
-      CF_PAGES_URL: z.string().optional(),
-    },
-    runtimeEnv,
-  });
-
-const numberish = z.union([z.number(), z.string().transform(Number)]);
---
+++ src/lib/env/preset/cloudflare.ts
@@ -0,0 +1,33 @@
+import { createEnv } from "@t3-oss/env-core";
+import z from "zod";
+
+type RuntimeEnv = Record<string, string | boolean | number | undefined>;
+
+/**
+ * @see https://developers.cloudflare.com/pages/configuration/build-configuration/#environment-variables
+ */
+export const cloudflare = (runtimeEnv: RuntimeEnv) =>
+  createEnv({
+    server: {
+      CI: z.stringbool().optional(),
+      NODE_ENV: z.enum(["development", "production"]).optional(),
+
+      WRANGLER_CI_GENERATE_PREVIEW_ALIAS: z.stringbool().optional(),
+      WRANGLER_CI_OVERRIDE_NAME: z.string().optional(),
+      WRANGLER_CI_MATCH_TAG: z.string().optional(),
+      WRANGLER_CI_OVERRIDE_NETWORK_MODE_HOST: z.stringbool().optional(),
+
+      WORKERS_CI: z
+        .string()
+        .transform((s) => parseInt(s, 10))
+        .pipe(z.number())
+        .optional(),
+      WORKERS_CI_BUILD_UUID: z.string().optional(),
+      WORKERS_CI_BRANCH: z.string().optional(),
+      WORKERS_CI_COMMIT_SHA: z.string().optional(),
+
+      CLOUDFLARE_ACCOUNT_ID: z.string().optional(),
+      CLOUDFLARE_API_TOKEN: z.string().optional(),
+    },
+    runtimeEnv,
+  });
