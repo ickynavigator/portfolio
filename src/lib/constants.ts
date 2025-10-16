9e4ba96ac0d76aeaf51104f5f310729118a5a868
Obi Fortune <gabrielobi.of@gmail.com>
2025-10-16 09:40:26 -0400

    update nav layout setup

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,3 +3,4 @@ export const PERSONAL_INFO_CONFIG_ID = "personalInfo";
 export const SANITY_PROJECT_ID = "gtsyvuts";
 export const SANITY_API_DATASET = "production";
 export const SANITY_API_VERSION = "2022-03-07";
+export const REPO_URL = "https://github.com/ickynavigator/portfolio/";
