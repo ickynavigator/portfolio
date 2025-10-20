400f8c827749b634a7db9175c28fba99febb43a3
Obi Fortune <gabrielobi.of@gmail.com>
2025-10-19 22:10:25 -0400

    update transport setup

--- party/transport.ts
+++ party/transport.ts
@@ -1,86 +1,132 @@
-import { z } from "zod";
+import * as z from "zod";
 
-const stringToJSONSchema = z.string().transform((value, ctx) => {
-  try {
-    return JSON.parse(value);
-  } catch {
-    ctx.addIssue({ code: "custom", message: "Invalid JSON" });
-    return z.NEVER;
-  }
-});
+class Transport<
+  Discriminant extends string,
+  SchemaUnion extends [
+    z.core.$ZodTypeDiscriminable,
+    ...z.core.$ZodTypeDiscriminable[],
+  ],
+> {
+  private schemaUnion: z.ZodDiscriminatedUnion<SchemaUnion, Discriminant>;
+  private discriminant: Discriminant;
 
-const JSONToStringSchema = z.any().transform((value, ctx) => {
-  try {
-    return JSON.stringify(value);
-  } catch {
-    ctx.addIssue({ code: "custom", message: "Invalid JSON" });
-    return z.NEVER;
-  }
-});
-
-export const MESSAGE_TYPES = {
-  message: "message",
-  bulk: "bulk",
-} as const;
-export type MESSAGE_TYPES = (typeof MESSAGE_TYPES)[keyof typeof MESSAGE_TYPES];
-
-const MessageSchema = z.object({
-  type: z.literal(MESSAGE_TYPES.message),
-  timestamp: z.number(),
-  message: z.string(),
-});
-
-const BulkSchema = z.object({
-  type: z.literal(MESSAGE_TYPES.bulk),
-  messages: z.array(MessageSchema),
-});
-
-export class Transport {
-  static SCHEMAS = {
-    message: MessageSchema,
-    bulk: BulkSchema,
-  } as const;
-
-  static COMBINED_SCHEMA = z.discriminatedUnion("type", [
-    Transport.SCHEMAS.message,
-    Transport.SCHEMAS.bulk,
-  ]);
-
-  tag<Tag extends keyof TransportTypes>(
-    type: Tag,
-    data: Omit<TransportTypes[Tag], "type">,
-  ) {
-    const tagged = Transport.SCHEMAS[type].parse({ type, ...data });
-
-    return tagged as TransportTypes[Tag];
+  constructor(config: {
+    schemaUnion: SchemaUnion;
+    discriminant: Discriminant;
+  }) {
+    this.schemaUnion = z.discriminatedUnion(
+      config.discriminant,
+      config.schemaUnion,
+    );
+    this.discriminant = config.discriminant;
   }
 
   encode(data: unknown) {
-    const parsed = JSONToStringSchema.parse(data);
-    return parsed;
+    const isValid = this.schemaUnion.safeParse(data);
+    if (!isValid.success) {
+      throw new Error("Invalid data");
+    }
+    const processed = JSON.stringify(isValid.data);
+    return {
+      processed,
+    };
   }
 
-  decode<T extends z.ZodTypeAny>(data: unknown, schema: T) {
-    const parsed = stringToJSONSchema.pipe(schema).parse(data);
-    return parsed as z.infer<T>;
+  decode(data: unknown) {
+    if (typeof data !== "string") {
+      throw new Error("Not a string");
+    }
+
+    const _parsed = JSON.parse(data);
+
+    const processed = this.schemaUnion.parse(_parsed);
+    return {
+      processed,
+    };
   }
 
-  match(data: string, cbs: Match) {
-    const parsed = this.decode(data, Transport.COMBINED_SCHEMA);
+  decodeByDiscriminant<
+    Tag extends DiscriminantOptions<typeof this.schemaUnion>,
+  >(data: unknown, discriminant: Tag) {
+    const _parsed = this.decode(data).processed;
+    const _discriminant = (_parsed as Record<Discriminant, unknown>)[
+      this.discriminant
+    ];
+    if (_discriminant !== discriminant) {
+      throw new Error("Invalid discriminant");
+    }
+    const parsed = _parsed as DataByDiscriminant<typeof this.schemaUnion, Tag>;
+    return {
+      processed: parsed,
+    };
+  }
 
-    type ParsedType = Extract<TransportTypes, { type: typeof parsed.type }>;
-    cbs[parsed.type]?.(parsed as ParsedType);
+  tag<Tag extends DiscriminantOptions<typeof this.schemaUnion>>(
+    discriminant: Tag,
+    data: Omit<DataByDiscriminant<typeof this.schemaUnion, Tag>, Discriminant>,
+  ) {
+    const tagged = this.schemaUnion.parse({
+      [this.discriminant]: discriminant,
+      ...data,
+    });
+
+    return {
+      tagged,
+      encoded: this.encode(tagged).processed,
+    };
+  }
+
+  match(cbs: MatchMap<typeof this.schemaUnion>) {
+    return (data: unknown) => {
+      const parsed = this.decode(data).processed;
+
+      type Casted = keyof MatchMap<typeof this.schemaUnion>;
+      type Parsed = Record<Discriminant, Casted>;
+
+      const discriminant = (parsed as Parsed)[this.discriminant];
+      if (!discriminant) {
+        throw new Error("Invalid data");
+      }
+
+      const callback = cbs[discriminant];
+      if (typeof callback !== "function") {
+        throw new Error(
+          `NO CALLBACK FOR DISCRIMINANT: ${discriminant.toString()}`,
+        );
+      }
+
+      return callback(parsed);
+    };
   }
 }
-type RecordOfZod = Record<string, z.ZodTypeAny>;
-type ZodWithLiteralType = z.ZodObject<{ type: z.ZodLiteral<string> }>;
 
-type Match = _Match<_Combined<typeof Transport.SCHEMAS>>;
-type TransportTypes = _Unwrapped<typeof Transport.SCHEMAS>;
-type _Unwrapped<T extends RecordOfZod> = { [K in keyof T]: z.infer<T[K]> };
-type _Combined<T extends RecordOfZod> = { [K in keyof T]: T[K] }[keyof T];
-type _Match<T extends ZodWithLiteralType> = {
-  [K in z.infer<T>["type"]]?: (data: Extract<z.infer<T>, { type: K }>) => void;
-};
+type DataByDiscriminant<
+  T extends z.ZodType,
+  Key extends DiscriminantOptions<T>,
+> =
+  T extends z.ZodDiscriminatedUnion<infer Items, infer Disc>
+    ? Disc extends keyof z.infer<Items[number]>
+      ? Extract<z.infer<Items[number]>, Record<Disc, Key>>
+      : never
+    : never;
+
+type DiscriminantOptions<T extends z.ZodType> =
+  T extends z.ZodDiscriminatedUnion<infer Items, infer Disc>
+    ? Disc extends keyof z.infer<Items[number]>
+      ? z.infer<Items[number]>[Disc]
+      : never
+    : never;
+
+type MatchMap<T extends z.ZodType> =
+  T extends z.ZodDiscriminatedUnion<infer Items, infer Disc>
+    ? {
+        [Key in Items[number] as z.infer<Key> extends Record<
+          Disc,
+          infer U extends PropertyKey
+        >
+          ? U
+          : never]: (data: z.infer<Key>) => void;
+      }
+    : never;
 
 export default Transport;
