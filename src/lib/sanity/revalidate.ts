9d351159a207386d0ca9aa76dfb9f97055d7d390
Obi Fortune <gabrielobi.of@gmail.com>
2026-05-12 16:23:20 -0400

    clone request for validate signature

--- src/lib/sanity/revalidate.ts
+++ src/lib/sanity/revalidate.ts
@@ -1,7 +1,9 @@
 import { isValidSignature, SIGNATURE_HEADER_NAME } from "@sanity/webhook";
 import { SANITY_REVALIDATE_SECRET } from "astro:env/server";
 
-export async function validateSignature(req: Request) {
+export async function validateSignature(_req: Request) {
+  const req = _req.clone();
+
   const signature = req.headers.get(SIGNATURE_HEADER_NAME);
   const stringifiedPayload = await req.text();
 
