90dde3da07fe43f92450352c3709299d36ada167
Obi Fortune <gabrielobi.of@gmail.com>
2026-07-17 12:52:38 -0400

    add revalidate setup

--- src/lib/sanity/revalidate.ts
+++ src/lib/sanity/revalidate.ts
@@ -1,7 +1,7 @@
 import { isValidSignature, SIGNATURE_HEADER_NAME } from "@sanity/webhook";
-import { SANITY_REVALIDATE_SECRET } from "astro:env/server";
+import z from "zod";
 
-export async function validateSignature(_req: Request) {
+export async function validateSignature(_req: Request, againstSecret: string) {
   const req = _req.clone();
 
   const signature = req.headers.get(SIGNATURE_HEADER_NAME);
@@ -11,9 +11,28 @@ export async function validateSignature(_req: Request) {
 
   const stringifiedPayload = await req.text();
 
-  return await isValidSignature(
-    stringifiedPayload,
-    signature,
-    SANITY_REVALIDATE_SECRET,
-  );
+  return await isValidSignature(stringifiedPayload, signature, againstSecret);
 }
+
+export const currentProjection = z.object({
+  _id: z.string(),
+  _type: z.string(),
+});
+
+export const getTagsFromBody = (body: z.infer<typeof currentProjection>) => {
+  const tags = [];
+
+  if (body._type) {
+    tags.push(`_type:${body._type}`);
+  }
+
+  if (body._id) {
+    tags.push(`_id:${body._id}`);
+  }
+
+  if (body._type && body._id) {
+    tags.push(`_type:${body._type}&_id:${body._id}`);
+  }
+
+  return tags;
+};
