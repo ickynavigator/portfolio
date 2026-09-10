48837eb7c8f1627e09d79ac77a11bd1c5c6dde21
Obi Fortune <gabrielobi.of@gmail.com>
2026-09-09 23:28:50 -0400

    test

--- src/lib/sanity/load-query.ts
+++ src/lib/sanity/load-query.ts
@@ -29,7 +29,7 @@ export async function loadQuery<G extends string>(
     _options = args.options;
   }
 
-  console.log(env);
+  console.log(Object.entries(env));
   if (env.PUBLIC_SANITY_VISUAL_EDITING_ENABLED == "true") {
     if (env.SANITY_API_READ_TOKEN == undefined) {
       throw new Error(
